f00198372551eddf3f110d96553bb05030eaa32f
mlgmontage <kettleofrassel1@gmail.com>
2020-04-28 22:53:33 -0700

    404 page and URL building

--- routes/cats.js
+++ routes/cats.js
@@ -5,4 +5,12 @@ router.get("/", function (req, res) {
   res.send("cats router home");
 });
 
+router.get("/:id([0-9]{3})", function (req, res) {
+  res.send(`individual cat ${req.params.id}`);
+});
+
+router.get("*", function (req, res) {
+  res.send("404 page");
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const cats = require("./routes/cats");
 
+// Third party middleware
+// app.use(bodyParser.json())
+
 // app.method(path, handler)
 app.get("/hello", (req, res) => {
   res.send("my first route");
